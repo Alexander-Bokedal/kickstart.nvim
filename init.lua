4ce2d2238e3ac846593a39daed7436dec878d500
Alexander-Bokedal <alexander.bokedal@gmail.com>
2025-01-18 11:23:13 +0100

    added TODO plugin

--- init.lua
+++ init.lua
@@ -193,6 +193,9 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 
 vim.keymap.set('n', '<leader>n', ':NvimTreeToggle<CR>', { noremap = true, silent = true, desc = 'Open filetree to the left' })
 
+-- Command for TODO
+
+vim.keymap.set('n', '<leader>T', ':TodoTelescope<CR>', { noremap = true, silent = true, desc = 'TodoTelescope' })
 --vim.keymap.set('n', 'C-n', ':NvimTreeToggle', { noremap = true, silent = true })
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
@@ -253,52 +256,12 @@ require('lazy').setup({
     end,
   },
   {
-    'pmizio/typescript-tools.nvim',
-    dependencies = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
+    'folke/todo-comments.nvim',
+    dependencies = { 'nvim-lua/plenary.nvim' },
     opts = {
-      settings = {
-        -- spawn additional tsserver instance to calculate diagnostics on it
-        separate_diagnostic_server = true,
-        -- "change"|"insert_leave" determine when the client asks the server about diagnostic
-        publish_diagnostic_on = 'insert_leave',
-        -- array of strings("fix_all"|"add_missing_imports"|"remove_unused"|
-        -- "remove_unused_imports"|"organize_imports") -- or string "all"
-        -- to include all supported code actions
-        -- specify commands exposed as code_actions
-        expose_as_code_action = {},
-        -- string|nil - specify a custom path to `tsserver.js` file, if this is nil or file under path
-        -- not exists then standard path resolution strategy is applied
-        tsserver_path = nil,
-        -- specify a list of plugins to load by tsserver, e.g., for support `styled-components`
-        -- (see 💅 `styled-components` support section)
-        tsserver_plugins = {},
-        -- this value is passed to: https://nodejs.org/api/cli.html#--max-old-space-sizesize-in-megabytes
-        -- memory limit in megabytes or "auto"(basically no limit)
-        tsserver_max_memory = 'auto',
-        -- described below
-        tsserver_format_options = {},
-        tsserver_file_preferences = {},
-        -- locale of all tsserver messages, supported locales you can find here:
-        -- https://github.com/microsoft/TypeScript/blob/3c221fc086be52b19801f6e8d82596d04607ede6/src/compiler/utilitiesPublic.ts#L620
-        tsserver_locale = 'en',
-        -- mirror of VSCode's `typescript.suggest.completeFunctionCalls`
-        complete_function_calls = false,
-        include_completions_with_insert_text = true,
-        -- CodeLens
-        -- WARNING: Experimental feature also in VSCode, because it might hit performance of server.
-        -- possible values: ("off"|"all"|"implementations_only"|"references_only")
-        code_lens = 'off',
-        -- by default code lenses are displayed on all referencable values and for some of you it can
-        -- be too much this option reduce count of them by removing member references from lenses
-        disable_member_code_lens = true,
-        -- JSXCloseTag
-        -- WARNING: it is disabled by default (maybe you configuration or distro already uses nvim-ts-autotag,
-        -- that maybe have a conflict if enable this feature. )
-        jsx_close_tag = {
-          enable = false,
-          filetypes = { 'javascriptreact', 'typescriptreact' },
-        },
-      },
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
     },
   },
   {
@@ -837,6 +800,7 @@ require('lazy').setup({
         -- You can use 'stop_after_first' to run the first available formatter from the list
         javascript = { 'prettierd', 'prettier', stop_after_first = true },
         typescript = { 'prettierd', 'prettier', stop_after_first = true },
+        html = { 'prettier' },
       },
     },
   },
@@ -908,13 +872,13 @@ require('lazy').setup({
           -- Accept ([y]es) the completion.
           --  This will auto-import if your LSP supports it.
           --  This will expand snippets if the LSP sent a snippet.
-          ['<C-y>'] = cmp.mapping.confirm { select = true },
+          ['<Tab>'] = cmp.mapping.confirm { select = true },
 
           -- If you prefer more traditional completion keymaps,
           -- you can uncomment the following lines
-          --['<CR>'] = cmp.mapping.confirm { select = true },
-          --['<Tab>'] = cmp.mapping.select_next_item(),
-          --['<S-Tab>'] = cmp.mapping.select_prev_item(),
+          -- ['<CR>'] = cmp.mapping.confirm { select = true },
+          -- ['<Tab>'] = cmp.mapping.select_next_item(),
+          -- ['<S-Tab>'] = cmp.mapping.select_prev_item(),
 
           -- Manually trigger a completion from nvim-cmp.
           --  Generally you don't need this, because nvim-cmp will display
@@ -1051,7 +1015,7 @@ require('lazy').setup({
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
-      ensure_installed = { 'typescript', 'javascript', 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
+      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
